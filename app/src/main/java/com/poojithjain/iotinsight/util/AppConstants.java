b172ef5bd7d6e2b42b04ce454309acbd01cab59f
Poojith <poojithjain@live.com>
2017-02-19 19:50:10 -0500

    Initial commit.

---
+++ app/src/main/java/com/poojithjain/iotinsight/util/AppConstants.java
@@ -0,0 +1,13 @@
+package com.poojithjain.iotinsight.util;
+
+/**
+ * Created by Poojith on 19-02-2017.
+ */
+
+public class AppConstants {
+    public static String[] tabTitles;
+
+    static {
+        tabTitles = new String[]{"Battery", "Sync", "Alarm"};
+    }
+}
