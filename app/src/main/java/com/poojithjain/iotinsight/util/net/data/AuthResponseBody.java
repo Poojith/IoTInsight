036bb7de686fa98ba37ee0f8801bc1ca6379a312
poojith <poojithjain@live.com>
2017-02-24 18:41:18 -0500

    Create POJO for AuthResponse.

---
+++ app/src/main/java/com/poojithjain/iotinsight/util/net/data/AuthResponseBody.java
@@ -0,0 +1,64 @@
+package com.poojithjain.iotinsight.util.net.data;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AuthResponseBody {
+
+    @SerializedName("access_token")
+    @Expose
+    private String accessToken;
+    @SerializedName("expires_in")
+    @Expose
+    private Integer expiresIn;
+    @SerializedName("refresh_token")
+    @Expose
+    private String refreshToken;
+    @SerializedName("token_type")
+    @Expose
+    private String tokenType;
+    @SerializedName("user_id")
+    @Expose
+    private String userId;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public Integer getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(Integer expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+}
---
+++ app/src/main/java/com/poojithjain/iotinsight/util/net/data/RequestType.java
@@ -0,0 +1,11 @@
+package com.poojithjain.iotinsight.util.net.data;
+
+/**
+ * Created by poojith on 2/24/17.
+ */
+
+public enum RequestType {
+    AccessToken,
+    Alarm,
+    Devices
+}
