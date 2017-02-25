10ac248aac50162cacf2c0ad383d5c21ffce71fa
poojith <poojithjain@live.com>
2017-02-25 16:43:03 -0500

    Update Async task with alarm and devices information.

--- app/src/main/java/com/poojithjain/iotinsight/util/net/api/FitbitAPITask.java
+++ app/src/main/java/com/poojithjain/iotinsight/util/net/api/FitbitAPITask.java
@@ -1,6 +1,7 @@
 package com.poojithjain.iotinsight.util.net.api;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.util.Log;
 import android.widget.Toast;
@@ -8,8 +9,10 @@ import android.widget.Toast;
 import com.poojithjain.iotinsight.util.app.AppConstants;
 import com.poojithjain.iotinsight.util.app.AppSharedPreferences;
 import com.poojithjain.iotinsight.util.net.data.AuthResponseBody;
+import com.poojithjain.iotinsight.util.net.data.FitbitAlarms;
 import com.poojithjain.iotinsight.util.net.data.FitbitDevice;
 import com.poojithjain.iotinsight.util.net.data.RequestType;
+import com.poojithjain.iotinsight.util.net.data.TrackerAlarm;
 
 import java.io.IOException;
 import java.util.HashMap;
@@ -26,6 +29,7 @@ public class FitbitAPITask extends AsyncTask {
     private Context context;
     private FitbitAPI api;
     private RequestType requestType;
+    private static int trackerID;
 
     public FitbitAPITask(Context context, RequestType requestType) {
         this.context = context;
@@ -34,45 +38,62 @@ public class FitbitAPITask extends AsyncTask {
 
     @Override
     protected void onPreExecute() {
-        api = AppConstants.getInstance(context).initAPI();
+        api = AppConstants.initAPI(context);
         super.onPreExecute();
     }
 
     @Override
     protected Object doInBackground(Object[] objects) {
         try{
+
             if (requestType == RequestType.Alarm) {
                 // TODO get trackerId
-                int trackerID = 0; // getTrackerId();
-                api.getAlarm(trackerID).execute();
+                Response<FitbitAlarms> alarms = api.getAlarm(trackerID).execute();
+                FitbitAlarms alarm = alarms.body();
+                TrackerAlarm trackerAlarm = alarm.getTrackerAlarms().get(0);
+                Log.i("Alarm time", trackerAlarm.getTime());
+                List<String> alarmDays = trackerAlarm.getWeekDays();
+                Log.i("Alarm days" , alarmDays.toString());
 
             } else if (requestType == RequestType.Devices) {
                 Response<List<FitbitDevice>> res = api.getDevices().execute();
-                Log.d("api", res.body().toString());
-                Toast.makeText(context, res.body().get(0).getDeviceVersion(), Toast.LENGTH_LONG).show();
+                List<FitbitDevice> devices = res.body();
+                FitbitDevice device = devices.get(0);
+                trackerID = Integer.parseInt(device.getId());
+                Log.i("Battery", device.getBattery());
+                Log.i("Sync time", device.getLastSyncTime());
+                Log.i("Tracker ID", String.valueOf(trackerID));
 
             } else if (requestType == RequestType.AccessToken) {
+                Thread.sleep(3000);
                 Map<String, String> body = new HashMap<>();
                 body.put("client_id", "2285P6");
                 body.put("grant_type", "authorization_code");
-                body.put("code", AppConstants.getInstance(context).getAuthorizationCode());
+                String authCode = AppSharedPreferences.getInstance().getSharedPreferences(context).getString("authCode", "").trim();
+                body.put("code", authCode);
+                Log.d("Check auth code", authCode);
 
                 Response<AuthResponseBody> authResponse = api.getTokens(body).execute();
                 AuthResponseBody responseBody = authResponse.body();
-                AppSharedPreferences.getInstance().editor(context).putString("accessToken",  responseBody.getAccessToken());
+
+                SharedPreferences.Editor editor = AppSharedPreferences.getInstance().editor(context);
+                editor.putString("accessToken", responseBody.getAccessToken());
                 Log.d("getAccessToken()", responseBody.getAccessToken());
-                AppSharedPreferences.getInstance().editor(context).putInt("expiresIn",  responseBody.getExpiresIn());
+                editor.putInt("expiresIn", responseBody.getExpiresIn());
                 Log.d("getExpiresIn()", String.valueOf(responseBody.getExpiresIn()));
-                AppSharedPreferences.getInstance().editor(context).putString("refreshToken",  responseBody.getRefreshToken());
+                editor.putString("refreshToken", responseBody.getRefreshToken());
                 Log.d("getRefreshToken()", responseBody.getRefreshToken());
-                AppSharedPreferences.getInstance().editor(context).putString("tokenType",  responseBody.getTokenType());
+                editor.putString("tokenType", responseBody.getTokenType());
                 Log.d("getTokenType()", responseBody.getTokenType());
-                AppSharedPreferences.getInstance().editor(context).putString("userId",  responseBody.getUserId());
+                editor.putString("userId", responseBody.getUserId());
                 Log.d("getUserId()", responseBody.getUserId());
-                Toast.makeText(context, "Did something", Toast.LENGTH_LONG).show();
+                editor.putBoolean("loginStatus", true);
+                editor.commit();
             }
         } catch (IOException ioe) {
             ioe.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         return null;
     }
